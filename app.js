6f730c8922d4a35abaca7f825bf30d041c40a02e
Hu <huang.h0709@gmail.com>
2023-06-01 15:34:07 -0400

    initial upload

---
+++ app.js
@@ -0,0 +1,81 @@
+// Morning Review
+
+//HTML structure --> Containerization
+//create boxes to organize elements
+
+//CSS positioning
+
+//DataTypes {Arrays, Boolean, Integer, String, Objects}
+
+//For Loops --> Conditionals:
+//Use () for functions/methods
+//Use [] for brackets
+//Use {} for code containment
+
+//Astronaut Practice:
+
+
+
+// let students = ["Matt","Hu","Sheroze"]
+// function addTextToName(studentName){
+//     students.forEach((kid)=>{
+//         console.log(kid, "is Awesome")
+//     })
+// }
+// addTextToName(students)
+// //--- Two ways to call function
+// students.forEach((student)=>{
+//     console.log(student, " is an incredible student")
+// })
+// //OR
+// for(i=0;i<students.length;i++){
+//     console.log(students[i], " is an incredible student")
+// }
+
+//two ways to do so:
+//function declaration:
+// function nameofFunc(){
+//     myFunc = ""
+// }
+// //arrow function //better way
+// const funcName = (/*parameters go here*/) =>{
+//     funky = ""
+// }
+
+// const getBreakFastItems = () => {
+//     let button = document.querySelector('#one')
+//     let items = prices.rice
+//     button.replaceWith("Menu now av...")
+//     items.forEach((i)=>{
+//         console.log(i)
+//         screen.append(i)
+//     })
+// }
+// getBreakFastItems()
+
+// const getAnyItem = (themes)=>{
+//     if(theme === "breakfast"){
+//         console.log(menu.breakfast.food)
+//     }
+//     else if(theme === "lunch"){
+//         console.log(menu.lunch.food)
+//     }
+//     else{
+//         console.log(menu.dinner.food)
+//     }
+// }
+
+let menu = {
+    locations: ["Brooklyn","Flushing","Manhattan"]
+}
+let restaurantName = "Restaurant Name";
+const riceItem = ["fried","steamed",]
+const noodleItem = ["fried","soup","cold"]
+const soupItem = ["fish","beef","lamb","chicken"]
+
+let prices={
+    rice:[9,8],
+    noodleItem:[9,9,10],
+    soup:[12,11,12,10]
+}
+
